9729ccbb0a8c3db5cc5c7075968ba27a91affc05
jwoodger <jwoodger@github.com>
2020-09-11 12:08:09 -0400

    Added exception to signal when client violates a contract.

---
+++ src/main/java/jwoodger/dottedline/PreconditionViolationException.java
@@ -0,0 +1,24 @@
+// Copyright (C) 2020 Jack Woodger
+// SPDX-License-Identifier: MPL-2.0
+package jwoodger.dottedline;
+
+/**
+ * Signifies when a client violates the precondition of a method.
+ */
+public class PreconditionViolationException extends RuntimeException {
+
+    /**
+     * Signals a precondition violation.
+     * @param message The detail message on the precondition.
+     */
+    public PreconditionViolationException(String message) {
+        super(message);
+    }
+
+    /**
+     * Signals a precondition violation with {@code null} as its message.
+     */
+    public PreconditionViolationException() {
+        super();
+    }
+}
