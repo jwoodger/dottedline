1b5564280e0d520ff85bc2f68ac04016b16676cd
jwoodger <jwoodger@github.com>
2020-09-11 14:42:57 -0400

    Added 'requires' methods to express pre-conditions.

---
+++ src/main/java/jwoodger/dottedline/Contracts.java
@@ -0,0 +1,38 @@
+package jwoodger.dottedline;
+
+/**
+ * Contains utility methods for expressing contracts in code.
+ */
+public final class Contracts {
+
+    private Contracts() {
+    }
+
+    /**
+     * Used to express a pre-condition, which is to say a condition imposed on the client of a
+     * method. Note that if assertions are disabled, this method does nothing.
+     * @param condition The condition which must be fulfilled.
+     * @param message A message to explain the condition. This is displayed if
+     * {@link PreconditionViolationException} that is thrown.
+     * @throws PreconditionViolationException if the condition is not met.
+     */
+    public static void requires(boolean condition, String message)
+        throws PreconditionViolationException {
+        try {
+            assert condition;
+        } catch (AssertionError err) {
+            if (message != null) {
+                throw new PreconditionViolationException(message);
+            }
+            throw new PreconditionViolationException();
+        }
+    }
+
+    /**
+     * Variant of "requires" that does not need a message.
+     * @see #requires(boolean, String)
+     */
+    public static void requires(boolean condition) throws PreconditionViolationException {
+        requires(condition, null);
+    }
+}
---
+++ src/test/java/jwoodger/dottedline/ContractsTest.java
@@ -0,0 +1,23 @@
+package jwoodger.dottedline;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static jwoodger.dottedline.Contracts.*;
+
+// Tests for the Contracts class.
+public class ContractsTest {
+
+    @DisplayName("requires does not throw given a passed condition.")
+    @Test
+    public void requiresDoesNotThrowOnSuccess() {
+        assertDoesNotThrow(() -> requires(true));
+    }
+
+    @DisplayName("requires throws a PreconditionViolationException given a failed condition")
+    @Test
+    public void requiresThrowsOnFailedCondition() {
+        assertThrows(PreconditionViolationException.class, () -> requires(false));
+    }
+}
